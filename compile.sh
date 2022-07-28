21ae77f2d4e99b0929522a33eb92fb3a16a33608
Theodor Totev <teoet6@protonmail.com>
2022-07-29 01:14:38 +0300

    update compile script

--- compile.sh
+++ compile.sh
@@ -3,16 +3,33 @@
 rm -rf bin
 mkdir -p bin
 
-for src in examples/*.c; do
-	out=$(basename "$src")
+buildc () {
+	out=$(basename "$1")
 	out="bin/${out%.*}"
-	echo "gcc $src -> $out"
-	gcc --std=c11 -Wall -Werror "$src" -lX11 -lGL -lm -O2 -o "$out"
+	echo "gcc $1 -> $out"
+	gcc --std=c11 -Wall -Werror "$1" -lX11 -lGL -lm -O2 -o "$out"
+}
+
+buildcpp () {
+	out=$(basename "$1")
+	out="bin/${out%.*}"
+	echo "g++ $1 -> $out"
+	g++ --std=c++11 -Wall -Werror "$1" -lX11 -lGL -lm -O2 -o "$out"
+}
+
+if [ -n "$1" ]; then
+	case ${1##*.} in
+		c)   buildc   "$1" ;;
+		cpp) buildcpp "$1" ;;
+		*)   echo "Don't know how to build $1." > /dev/stderr ; exit 1 ;;
+	esac
+	exit 0
+fi
+
+for src in examples/*.c; do
+	buildc "$src"
 done
 
 for src in examples/*.cpp; do
-	out=$(basename "$src")
-	out="bin/${out%.*}"
-	echo "g++ $src -> $out"
-	g++ --std=c++11 -Wall -Werror "$src" -lX11 -lGL -lm -O2 -o "$out"
+	buildcpp "$src"
 done
