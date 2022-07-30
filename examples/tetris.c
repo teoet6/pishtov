d715daa807ec26d230c0fff5b129f0c4633025f8
Theodor Totev <teoet6@protonmail.com>
2022-07-30 15:44:37 +0300

    Add holding and next block preview

--- examples/tetris.c
+++ examples/tetris.c
@@ -5,6 +5,8 @@
 #define board_h 20
 #define board_w 10
 
+#define next_len 3
+
 #define block_ids 7
 
 bool board[board_w][board_h];
@@ -18,7 +20,7 @@ struct Block_Definition {
 
 typedef struct Block Block;
 struct Block {
-	uint64_t id;
+	int64_t id;
 	int64_t x;
 	int64_t y;
 	int64_t rot;
@@ -45,34 +47,39 @@ Block_Definition block_defs[block_ids] = {
 	},
 	// O
 	{
-		.x = { { 0, 1, 1, 0 }, { 0, 1, 1, 0 }, { 0, 1, 1, 0 }, { 0, 1, 1, 0 } },
-		.y = { { 0, 0, 1, 1 }, { 0, 0, 1, 1 }, { 0, 0, 1, 1 }, { 0, 0, 1, 1 } },
+		.x = { { 0, -1, -1,  0 }, { 0, -1, -1,  0 }, { 0, -1, -1,  0 }, { 0, -1, -1,  0 } },
+		.y = { { 0,  0, -1, -1 }, { 0,  0, -1, -1 }, { 0,  0, -1, -1 }, { 0,  0, -1, -1 } },
 		.color = 0x00ffff,
 	},
 	// S
 	{
-		.x = { { 1, 0, 0, -1 }, { 0, 0, -1, -1 }, { 1, 0, 0, -1 }, { 0, 0, -1, -1 } },
-		.y = { { 0, 0, 1,  1 }, { 1, 0,  0, -1 }, { 0, 0, 1,  1 }, { 1, 0,  0, -1 } },
+		.x = { {  1,  0, 0, -1 }, { 1, 1, 0,  0 }, {  1,  0, 0, -1 }, { 1, 1, 0,  0 } },
+		.y = { { -1, -1, 0,  0 }, { 1, 0, 0, -1 }, { -1, -1, 0,  0 }, { 1, 0, 0, -1 } },
 		.color = 0xff0000,
 	},
 	// T
 	{
-		.x = { { 0, -1, 0, 1 }, { 0,  0, -1, 0 }, { 0, 1,  0, -1 }, { 0, 0, 1,  0 } },
-		.y = { { 0,  0, 1, 0 }, { 0, -1,  0, 1 }, { 0, 0, -1,  0 }, { 0, 1, 0, -1 } },
+		.x = { { 0, 1,  0, -1 }, { 0, 0, 1,  0 }, { 0, -1, 0, 1 }, { 0,  0, -1, 0 } },
+		.y = { { 0, 0, -1,  0 }, { 0, 1, 0, -1 }, { 0,  0, 1, 0 }, { 0, -1,  0, 1 } },
 		.color = 0xff00ff,
 	},
 	// Z
 	{
-		.x = { { -1, 0, 0,  1 }, {  0, 0, -1, -1 }, { -1, 0, 0,  1 }, {  0, 0, -1, -1 } },
-		.y = { {  0, 0, 1,  1 }, { -1, 0,  0,  1 }, {  0, 0, 1,  1 }, { -1, 0,  0,  1 } },
+		.x = { { -1,  0, 0, 1 }, {  1, 1, 0, 0 }, { -1,  0, 0, 1 }, {  1, 1, 0, 0 } },
+		.y = { { -1, -1, 0, 0 }, { -1, 0, 0, 1 }, { -1, -1, 0, 0 }, { -1, 0, 0, 1 } },
 		.color = 0xffff00,
 	},
 };
 
+bool can_hold = true;
+int64_t hold_id = -1;
+int64_t next_ids[next_len];
 Block block;
 
 void save_block() {
-	for (uint64_t i = 0; i < 4; i += 1) {
+	can_hold = true;
+
+	for (int64_t i = 0; i < 4; i += 1) {
 		int64_t x = block.x + block_defs[block.id].x[block.rot][i];
 		int64_t y = block.y + block_defs[block.id].y[block.rot][i];
 		board[x][y] = true;
@@ -97,7 +104,30 @@ void save_block() {
 }
 
 void next_block() {
-	block.id = rand() % block_ids;
+	block.id = next_ids[0];
+	block.rot = 0;
+	block.x = 4;
+	block.y = -2;
+
+	for (int64_t i = 1; i < next_len; i += 1) {
+		next_ids[i - 1] = next_ids[i];
+	}
+	next_ids[next_len - 1] = rand() % block_ids;
+}
+
+void hold_block() {
+	can_hold = false;
+
+	if (hold_id == -1) {
+		hold_id = block.id;
+		next_block();
+		return;
+	}
+
+	int64_t tmp_id = hold_id;
+	hold_id = block.id;
+
+	block.id = tmp_id;
 	block.rot = 0;
 	block.x = 4;
 	block.y = -2;
@@ -105,11 +135,14 @@ void next_block() {
 
 void init() {
 	srand(time(0));
+	for (int64_t i = 0; i < next_len; i += 1) {
+		next_ids[i] = rand() % block_ids;
+	}
 	next_block();
 }
 
 bool block_conflicts(Block b) {
-	for (uint64_t i = 0; i < 4; i += 1) {
+	for (int64_t i = 0; i < 4; i += 1) {
 		int64_t x = b.x + block_defs[b.id].x[b.rot][i];
 		int64_t y = b.y + block_defs[b.id].y[b.rot][i];
 
@@ -134,7 +167,7 @@ void tick() {
 }
 
 void update() {
-	static uint64_t updates;
+	static int64_t updates;
 	if (updates == 0) tick();
 	updates += 1;
 	updates %= 30;
@@ -142,14 +175,17 @@ void update() {
 
 void draw() {
 	{
-		float scale_to_fit_w = window_w / board_w;
-		float scale_to_fit_h = window_h / board_h;
+		float needed_w = board_w + 5;
+		float needed_h = board_h;
+
+		float scale_to_fit_w = window_w / needed_w;
+		float scale_to_fit_h = window_h / needed_h;
 		float scale_to_fit = fminf(scale_to_fit_w, scale_to_fit_h);
 		scale(scale_to_fit, scale_to_fit);
 	}
 
-	for (uint64_t x = 0; x < board_w; x += 1) {
-		for (uint64_t y = 0; y < board_h; y += 1) {
+	for (int64_t x = 0; x < board_w; x += 1) {
+		for (int64_t y = 0; y < board_h; y += 1) {
 			if (board[x][y]) fill_color(0x808080);
 			else             fill_color(0xf0f0f0);
 			fill_rect(x, y, .95f, .95f);
@@ -164,7 +200,7 @@ void draw() {
 		ghost.y -= 1;
 
 		fill_color(0xffffff);
-		for (uint64_t i = 0; i < 4; i += 1) {
+		for (int64_t i = 0; i < 4; i += 1) {
 			float x = ghost.x + block_defs[ghost.id].x[ghost.rot][i];
 			float y = ghost.y + block_defs[ghost.id].y[ghost.rot][i];
 			fill_rect(x, y, .95f, .95f);
@@ -172,10 +208,26 @@ void draw() {
 	}
 
 	fill_color(block_defs[block.id].color);
-	for (uint64_t i = 0; i < 4; i += 1) {
+	for (int64_t i = 0; i < 4; i += 1) {
 		float x = block.x + block_defs[block.id].x[block.rot][i];
 		float y = block.y + block_defs[block.id].y[block.rot][i];
 		fill_rect(x, y, .95f, .95f);
+	}
+
+	fill_color(block_defs[hold_id].color);
+	for (int64_t i = 0; i < 4; i += 1) {
+		float x = board_w + 2 + block_defs[hold_id].x[0][i];
+		float y = board_h - 2 + block_defs[hold_id].y[0][i];
+		fill_rect(x, y, .95f, .95f);
+	}
+
+	for (int64_t i = 0; i < next_len; i += 1) {
+		fill_color(block_defs[next_ids[i]].color);
+		for (int64_t j = 0; j < 4; j += 1) {
+			float x = board_w + 2 + block_defs[next_ids[i]].x[0][j];
+			float y = i * 3 + 3   + block_defs[next_ids[i]].y[0][j];
+			fill_rect(x, y, .95f, .95f);
+		}
 	}
 }
 
@@ -194,6 +246,10 @@ void keydown(int key) {
 		else block.rot += 1;
 		if (block_conflicts(block)) block.rot = prev;
 		return;
+	}
+
+	if (key == 'C') {
+		if (can_hold) hold_block();
 	}
 
 	if (key == 37) {
